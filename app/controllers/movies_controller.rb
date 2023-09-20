d4f94af2f99bcd0d9818191258c4331c15851eb8
ellsa-leung <wmleung74@email.com>
2023-09-19 22:40:48 -0500

    Added styling with Bulma

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,15 @@
 class MoviesController < ApplicationController
+  # Show all data on movies' page
   def index
+    # this create N + 1 problem
+    # +1 refers to movies whereas N refers to production_companies
+    # in short this means we are reduntantly calling the databse which can be expensive :)
+    # @movies = Movie.all
+    @movies = Movie.includes(:production_company).all.order('average_vote DESC')
   end
 
+  # show single data on each movie's page
   def show
+    @movie = Movie.find(params[:id])
   end
 end
