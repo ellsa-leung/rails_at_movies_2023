85a36f06f03cd1c779b6dbfeaf690cce37a25a98
ellsa-leung <wmleung74@email.com>
2023-09-19 21:33:50 -0500

    modified the seed script to pull in movie data

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,11 @@ require 'csv'
 Movie.delete_all
 ProductionCompany.delete_all
 
+# Reset the auto-increement counter for the primary key (id)
+# This step is specific to SQLite and may not work with other databases
+# Replace 'your_models' with the actual name of your table
+ActiveRecord::Base.connection.execute("Delete FROM sqlite_sequence WHERE name='production_companies';")
+
 #open top_movies.csv
 filename = Rails.root.join("db/top_movies.csv")
 
@@ -17,6 +22,16 @@ movies.each do |m|
 
   if production_company && production_company.valid?
     # create the movie
+    movie = production_company.movies.create(
+      title: m['original_title'],
+      year: m['year'],
+      duration: m['duration'],
+      description: m['description'],
+      average_vote: m['avg_vote']
+    )
+
+    puts "Invalid movie #{m['original_title']}" unless movie&.valid?
+
   else
     puts "invalid production company #{m['production_company']} for movie #{m['original_title']}"
   end
@@ -24,3 +39,4 @@ movies.each do |m|
 end
 
 puts "Created #{ProductionCompany.count} Production Companies"
+puts "Created #{Movie.count} Movies."
